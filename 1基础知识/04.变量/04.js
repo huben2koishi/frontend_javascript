3bb3937d4b0434e110ef22ad086361e1d12b682e
Koishi <huben2koishi@gmail.com>
2019-03-10 15:14:59 +0800

    update

---
+++ 1基础知识/04.变量/04.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// 使用 let 创建变量
+let message;
+message = 'hello';
+alert(message);
+
+// 同时声明多个变量时建议拆分为多行
+let user = 'John',
+    age = '22',
+    job = 'driver';
+
+// 变量名称仅能包含: 数字 字母 $ _
+// 且首字符不能为数字
+
+// 常量用 const 声明
+const MY_BIRTHDAY = '1997-05-16';
+// 常量声明后不可再更改
+// 通常常量名使用大写字母
