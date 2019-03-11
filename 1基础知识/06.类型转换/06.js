bf6bd5b808d193a8cc21ff386f26e70a032f54dd
Koishi <huben2koishi@gmail.com>
2019-03-11 16:29:46 +0800

    update

--- 1基础知识/06.类型转换/06.js
+++ 1基础知识/06.类型转换/06.js
@@ -8,11 +8,19 @@ value = String(value);
 console.log(typeof value);
 
 // toNumber 在算术函数和表达式中会自动进行 number 转换
+// undefined   NaN
+// null        0
+// true /false 1
+// string      从字符串中读出数字, 空字符串为 0, 错误返回 NaN
 console.log('6' / '2');
+// 也可以显式调用 Number(value), 如果字符串不是一个有效的数字则会返回 NaN
+let str = "123";
+console.log(typeof str);
+let num = Number(str);
+console.log(typeof num);
 
-
-// share 网易云音乐 tim 微信 贴吧lite 酷安 bilibili anitama via vpn
-// 一个木函 卡吧价钱 账号本子 支付宝 淘宝 京东 12306 mm  存储重定向 黑域
-// re管理器 山寨云 adm 阅读 z直播 便签 taptap youtube twitter pxez picacg
-// 异次元
-//
+// toBoolean 转换为 boolean 类型
+// 0 '' null undefined NaN 转为 false, 其它转为 true
+console.log(Boolean(1));
+console.log(Boolean('hello'));
+console.log(Boolean(''));
---
+++ 1基础知识/07.运算符/07.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// + 字符串连接符
+let s = "my" + "string";
+console.log(s);
