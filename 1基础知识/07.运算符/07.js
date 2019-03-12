6ca5da76e6a0c8fe743f59bdd4f07b42df413a8d
Koishi <huben2koishi@gmail.com>
2019-03-12 10:18:08 +0800

    update

--- 1基础知识/07.运算符/07.js
+++ 1基础知识/07.运算符/07.js
@@ -1,5 +1,12 @@
 "use strict";
 
-// + 字符串连接符
+// 字符串连接符 +
 let s = "my" + "string";
-console.log(s);
+console.log(s);       // mystring
+console.log(2+2+'1'); // 41
+
+// 赋值运算符 =
+// 取余运算符 %
+// 幂运算符 **
+// 自运算符 ++ --
+// 位运算符 $ | ^ ~ << >> >>
