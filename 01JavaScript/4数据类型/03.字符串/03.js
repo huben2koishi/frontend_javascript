ad2b94d9a4f3c22fa03c8a33e58d88fa659e24fc
Koishi <huben2koishi@gmail.com>
2019-03-27 23:23:11 +0800

    update

--- 01JavaScript/4数据类型/03.字符串/03.js
+++ 01JavaScript/4数据类型/03.字符串/03.js
@@ -31,3 +31,37 @@ console.log(single.charAt(single.length-1));
 let str = 'Widget with id';
 console.log(str.indexOf('Widget')); // 0
 console.log(str.indexOf('widget')); // -1 检索大小写敏感
+
+// str.lastIndexOf(substr, pos) 从字符串末尾开始搜索
+// str.includes(substr, pos) 返回str是否包含substr
+// str.startsWith(substr) 返回str是否以substr开头
+// str.endsWith(substr) 返回str是否以substr结尾
+
+// 获取子字符串 substring substr slice
+// str.slice(start [, end]) 返回str从start到end(不包括)的部分
+let str1 = 'stringify';
+console.log(str1.slice(0,5)); // strin
+// start end 可为负值, 意为从字符串结尾开始计算
+
+// str.substring(start [, end]) 同slice, 但它允许end大于start
+console.log(str1.substring(5,1)); // trin
+
+// str.substr(start [, length]) 返回str从start开始长length的部分
+console.log(str1.substr(2,4)); //ring
+
+// 建议使用 slice
+
+// str.codePointAt(pos) 返回字符串pos位置的字符代码
+console.log('a'.codePointAt(0)); // 97
+console.log('A'.codePointAt(0)); // 65
+
+// str.fromCodePoint(code) 通过数字code创建字符
+console.log(String.fromCodePoint(90)); // Z
+// 90的16进制为5a, 还可用 \u005a 表示 Z
+console.log('\u005a');
+
+// 字符串的比较
+// 1. 小写字母 > 大写字母
+// 2. 带有指示性标记的字母"不正常", 如 Ö
+// str1.localeCompare(str2) 大于返回1 小于返回-1 等于返回0
+console.log('asd'.localeCompare('b')); // -1
