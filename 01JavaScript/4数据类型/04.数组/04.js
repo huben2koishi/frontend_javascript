3d34dfeff0769ec2aab0961740391496c4d1c68d
Koishi <huben2koishi@gmail.com>
2019-03-28 22:27:19 +0800

    update

--- 01JavaScript/4数据类型/04.数组/04.js
+++ 01JavaScript/4数据类型/04.数组/04.js
@@ -1,2 +1,34 @@
 "use strict";
 
+// 声明数组
+let arr1 = new Array();
+let arr2 = []; // 推荐使用 []
+
+let fruits = ['apple', 'banana', 'orange'];
+console.log(fruits);
+fruits[1] = 'pear';
+console.log(fruits.length);
+
+// pop() 末端取出一个元素 push(item) 末端添加一个元素
+fruits.pop();
+console.log(fruits);
+fruits.push('lemon');
+console.log(fruits);
+
+// shift() 前端取出一个元素 unshift(item) 前端添加一个元素
+fruits.shift();
+console.log(fruits);
+fruits.unshift('berry');
+console.log(fruits);
+
+// push 和 unshift 均可一次添加多个元素
+
+// 循环数组 for...of...
+for (let item of fruits) {
+    console.log(item);
+}
+
+// length 数组最大索引值+1 手动减小length, 数组本身也会变小
+let arr = [1, 2, 3, 4, 5];
+arr.length=2;
+console.log(arr); // 1 2
