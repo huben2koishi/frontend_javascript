2187064a6989209d84bf815b45b5da434c9e06bc
Koishi <huben2koishi@gmail.com>
2019-04-02 16:12:41 +0800

    update

--- 01JavaScript/4数据类型/06.可迭代对象/06.js
+++ 01JavaScript/4数据类型/06.可迭代对象/06.js
@@ -1,2 +1,56 @@
 "use strict";
 
+// 为使得对象可迭代, 需添加 Symbol.iterator 方法
+// 该方法返回一个对象 它必须有一个 next() 方法
+// 当 for...of 循环希望取得下一个值时, 就会调用这个 next() 方法
+// next() 方法返回结果格式必须为 {done: boolean, value: any}
+let range = {
+    first: 1,
+    second: 2,
+    third: 3,
+    forth: 4,
+    fifth: 5,
+
+    [Symbol.iterator]() {
+        return {
+            current: this.first,
+            last: this.fifth,
+            next() {
+                if (this.current <= this.last) {
+                    return {done: false, value: this.current++};
+                } else {
+                    return {done: true};
+                }
+            }
+        }
+    },
+};
+for (let item of range) {
+    console.log(item);
+}
+
+// 字符串和数组都是可迭代的
+for (let char of 'hello') {
+    console.log(char);
+}
+
+// 显式调用迭代器
+let iterator = 'world'[Symbol.iterator]();
+
+while (true) {
+    let result = iterator.next();
+    if (result.done) break;
+    console.log(result.value);
+}
+
+// Iterables 应用于 Symbol.iterator 方法的对象
+// Array-likes 有索引和 length 属性的对象
+let arrayLike = {
+    0: 'hello',
+    1: 'world',
+    length: 2,
+};
+
+// Array.from() 将一个可迭代对象或类数组对象转化为数组
+let arr = Array.from(arrayLike);
+console.log(arr.pop());
