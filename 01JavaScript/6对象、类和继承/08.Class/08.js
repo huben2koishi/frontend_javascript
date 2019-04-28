eed8739c75d50eaabd45c5630856750ddf2992a7
koishi <huben2koishi@gmail.com>
2019-04-28 17:33:59 +0800

    update

--- 01JavaScript/6对象、类和继承/08.Class/08.js
+++ 01JavaScript/6对象、类和继承/08.Class/08.js
@@ -32,3 +32,31 @@ class User2 {
         this.name = value;
     }
 }
+
+// class 中只能定义方法, 若想添加一些非函数类型的值, 需手动更改 prototype
+class User3 {
+}
+
+User3.prototype.test = 5;
+console.log(new User3().test);
+
+// class 表达式
+function makeClass(phrase) {
+    return class {
+        sayHi() {
+            console.log(phrase);
+        }
+    }
+}
+
+let User4 = makeClass('hello');
+
+new User4().sayHi();
+
+// 静态方法
+class User5 {
+    static staticMethod() {
+        console.log(this == User5);
+    }
+}
+User5.staticMethod();
---
+++ 01JavaScript/6对象、类和继承/09.类继承和super/09.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// 继承 extends
+class Animal {
+    constructor(name) {
+        this.speed = 0;
+        this.name = name;
+    }
+
+    run(speed) {
+        this.speed += speed;
+        console.log(`${this.name} runs with speed ${this.speed}`);
+    }
+
+    stop() {
+        this.speed = 0;
+        console.log(`${this.name} stopped`);
+    }
+}
+
+class Rabbit extends Animal {
+    hide(){
+        console.log(`${this.name} hides`)
+    }
+}
